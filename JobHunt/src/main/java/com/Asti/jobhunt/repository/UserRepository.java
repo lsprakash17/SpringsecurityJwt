3f91ce80c175dd97299e0e462d57065f08ad7349
Prakash M <130843354+lsprakash17@users.noreply.github.com>
2024-04-17 18:22:18 +0530

    Add files via upload

---
+++ JobHunt/src/main/java/com/Asti/jobhunt/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.Asti.jobhunt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Asti.jobhunt.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+
+	boolean existsByUsername(String username);
+
+	boolean existsByEmail(String email);
+
+}
